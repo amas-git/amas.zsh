fbedeb6c5d3edfc5c462a93a6bf1cc9ef02e7625
amas <zhoujb.cn@gmail.com>
2013-05-14 18:55:13 +0800

    refactory pattern hunter

---
+++ _zsh.d/libs/libxml.zsh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+typeset -A XML_ATTRS
+alias xml.edit.subnode="xmlstarlet ed -P -L -s"
+alias xml.edit.update="xmlstarlet ed -P -L -u"
+
+alias xml.listE="xmlstarlet el"
+alias xml.listEA="xmlstarlet el -a"
+alias xml.listEAV="xmlstarlet el -v"
+
+
+
+
+
+
+
+
+
+
+
+
+
