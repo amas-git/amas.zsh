222fd18210b76d71cc48fb6487dee516dc97d8df
amas <zhoujb.cn@gmail.com>
2019-05-10 20:20:52 +0800

    added base58

--- _zsh.d/libs/misc.zsh
+++ _zsh.d/libs/misc.zsh
@@ -609,13 +609,3 @@ function ustd.getTx() {
 #interval - average time between blocks in seconds
 #eta - estimated time until the next block (in seconds)
 #avgtxnumber - 
-
-function base58() {
-    local input=$(<&0)
-    local x
-    local dict='123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
-
-    for x in $(echo -n $input | od -An -b); do
-        print $x
-    done
-}
