d6458aa7c30efad2feab9d672f0e4306171470a1
amas <amas@amasdeMac-mini.local>
2024-01-09 08:59:40 +0800

    add bseo command

---
+++ _zsh.d/libs/bseo.sh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+
+
+BSEO_BASE='bitrue.com'
+BSEO_MAX=3
+
+function main() {
+    local install=$(whence googler)
+    [[ -z $install ]] && {
+       print " googler command NOT FOUND, see: https://github.com/jarun/googler" 
+       return -1
+    }
+
+    bseo.urls $argv
+
+}
+
+function bseo.urls() {
+    local xs=(${argv:=$(<&0)})
+    
+    for x in $xs; do
+        search="site:$BSEO_BASE/$x"
+        for n in {0..1}; do
+            s=$[n*100+1]
+            n=$[n+100+100]
+            sleep 1
+            googler -s $s -n $n --json "search" | jq '.[].url'
+        done
+    done
+}
+
+
+
+main "$@"
